229f58f0fa9103fb416d92e178d4e14ce438dcd0
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-12 17:34:43 +0200

    update

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,8 +4,11 @@ import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar';
 import { useCallback, useState } from 'react'
 import MenuItem from './MenuItem';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 
 const UserMenu = () => {
+
+    const RegisterModal = useRegisterModal();
 
     const [isOpen, setIsOpen] = useState(false)
 
@@ -31,7 +34,7 @@ const UserMenu = () => {
                 <div className='flex flex-col cursor-pointer'>
                     <>
                         <MenuItem onClick={() => {}} label='Login' />
-                        <MenuItem onClick={() => {}} label='Sign Up' />
+                        <MenuItem onClick={RegisterModal.onOpen} label='Sign Up' />
                     </>
                 </div>
             </div>
