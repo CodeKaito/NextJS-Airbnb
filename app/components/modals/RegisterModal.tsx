743344b4ea89bff0a925be2145c5ab7037ccf77e
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-13 13:11:37 +0200

    update

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -7,6 +7,8 @@ import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
+import Heading from '../Heading';
+import Input from '../inputs/Input';
 
 const RegisterModal = () => {
 
@@ -39,7 +41,19 @@ const RegisterModal = () => {
 
     const bodyContent = (
         <div className='flex flex-col gap-4'>
-            Hello Modal Body!
+            <Heading
+                center
+                title='Welcome to Airbnb'
+                subtitle='Create an account!'
+            />
+            <Input
+                id="email"
+                label="email"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+                />
         </div>
     )
 
